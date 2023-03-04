93ff8aa4baf1c6c2c733a405e8f2f2ab2158c922
aravindaka14 <aravindaka14@gmail.com>
2023-03-04 18:29:40 +0530

    first commit

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 50
+    },
+    lastName: {
+        type: String,
+        min: 2,
+        max: 50,
+        required: true
+    },
+    email: {
+        type: String,
+        mzx: 50,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 5
+    },
+    picturePath: {
+        type: String,
+        default: ""
+    },
+    friends: {
+        type: Array,
+        default: []
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+},
+    { timestamps: true })
+
+const User = mongoose.model("Users", UserSchema)
+
+export default User;
